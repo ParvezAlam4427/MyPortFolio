c2c53d410fccc2ceca12ae4b513148b1108e5fcb
ParvezAlam4427 <168392194+ParvezAlam4427@users.noreply.github.com>
2025-11-15 10:05:11 +0530

    Update ContactController.java

--- src/main/java/com/contact/MyPortfolio/controller/ContactController.java
+++ src/main/java/com/contact/MyPortfolio/controller/ContactController.java
@@ -1,23 +1,22 @@
 package com.contact.MyPortfolio.controller;
 
-import com.contact.MyPortfolio.entity.ContactMessage;
-import com.contact.MyPortfolio.service.ContactMessageService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.contact.MyPortfolio.entity.ContactMessage;
+import com.contact.MyPortfolio.service.ContactMessageService;
 
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = "*")  // allow HTML/JS requests
+@CrossOrigin(origins = "*") // allow frontend access from anywhere
 public class ContactController {
 
     @Autowired
     private ContactMessageService service;
 
     @PostMapping
-    public ResponseEntity<String> submitMessage(@RequestBody ContactMessage msg) {
-        service.save(msg);
-        return ResponseEntity.ok("Message saved successfully!");
+    public ResponseEntity<String> submitMessage(@RequestBody ContactMessage message) {
+        service.saveMessage(message);
+        return ResponseEntity.ok("Message Saved Successfully");
     }
 }
