3bafe6ad87b47aaa5e4dee672862613792a06df1
ParvezAlam4427 <168392194+ParvezAlam4427@users.noreply.github.com>
2025-11-15 10:31:13 +0530

    Update ContactController.java

--- src/main/java/com/contact/MyPortfolio/controller/ContactController.java
+++ src/main/java/com/contact/MyPortfolio/controller/ContactController.java
@@ -1,18 +1,16 @@
-package com.contact.MyPortfolio.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import com.contact.MyPortfolio.entity.ContactMessage;
-import com.contact.MyPortfolio.service.ContactMessageService;
-
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = "*") // allow frontend access from anywhere
+@CrossOrigin(origins = "*")
 public class ContactController {
 
     @Autowired
     private ContactMessageService service;
+
+    // Test endpoint for browser
+    @GetMapping("/test")
+    public String test() {
+        return "Backend is working!";
+    }
 
     @PostMapping
     public ResponseEntity<String> submitMessage(@RequestBody ContactMessage message) {
